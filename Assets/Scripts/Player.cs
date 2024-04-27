ae81e0177ba0208186910779f2bdbeeaca96c86d
Matheus <matheussantosmoura2000@gmail.com>
2024-04-27 16:28:56 -0300

    System collision prototype

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,11 @@ public class Player : MonoBehaviour
 	private void Update() {
 		Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 		Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-		transform.position += moveDir * moveSpeed * Time.deltaTime;
+		float playerSize = .7f;
+		bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
+		if (canMove){
+			transform.position += moveDir * moveSpeed * Time.deltaTime;
+		}
 		isWalking = moveDir != Vector3.zero;
 		transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
 
