057d8abbe1dea80878915e3eef4f2e036060f79e
Matheus <matheussantosmoura2000@gmail.com>
2024-05-10 18:51:47 -0300

    Selected Counter Visual Implemented

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,36 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {	
+	// Properties
+	public static Player Instance {get; private set;}
+
+	public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+	public class OnSelectedCounterChangedEventArgs : EventArgs {
+		public ClearCounter selectedCounterI;
+	}
 	private bool isWalking;
 	private Vector3 lastInteractDir;
+	private ClearCounter selectedCounter;
+
 	[SerializeField] private float moveSpeed = 5f;
 	[SerializeField] private float rotationSpeed = 10f;
 	[SerializeField] private GameInput gameInput;
+
+	// Nunca ter +1 player
+	private void Awake(){
+		if(Instance != null){
+			Debug.LogError("Mais de uma instância de Player encontrada!");
+		}
+			Instance = this;
+		
+	}
 
 	private void Start(){
 		gameInput.onInteractAction += GameInput_OnInteractAction;
 	}
 
 	private void GameInput_OnInteractAction(object sender, System.EventArgs e){
-		Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-
-		Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-
-		float interactDistance = 2f;
-		if(moveDir != Vector3.zero){
-			lastInteractDir = moveDir;
-		}
-
-		// out significa que essa variavel armazena o retorno da função
-		if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance)){
-			// TryGetComponent já lida com valores null
-			if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
-				// Há um clearCounter na frente do player
-				clearCounter.Interact();
-			}
+		if(selectedCounter != null){
+			selectedCounter.Interact();
 		}
 	}
 
@@ -54,9 +59,15 @@ public class Player : MonoBehaviour
 			// TryGetComponent já lida com valores null
 			if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
 				// Há um clearCounter na frente do player
+				if (clearCounter != selectedCounter){
+					SetSelectedCounter(clearCounter);
+				}
+			} else {
+				SetSelectedCounter(null);
 			}
+		} else {
+			SetSelectedCounter(null);
 		}
-
 
 	}
 
@@ -96,5 +107,12 @@ public class Player : MonoBehaviour
 
 	public bool IsWalking(){
 		return isWalking;
+	}
+
+	private void SetSelectedCounter(ClearCounter selectedCounter){
+		this.selectedCounter = selectedCounter;
+		OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs{
+			selectedCounterI = selectedCounter
+		});
 	}
 }
